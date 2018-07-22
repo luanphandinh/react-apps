3de2cd1ba28e4fa39bd640746424e9db33d76aed
Luan Phan <luan.phan@go1.com>
2018-07-22 17:55:08 +0700

    [#movie-app] add infinite scroll to explore page

--- src/apps/movie-app/components/explore-page.tsx
+++ src/apps/movie-app/components/explore-page.tsx
@@ -1,14 +1,23 @@
 import * as React from 'react';
 
+import { HttpClient } from 'lupa/utils/http-client';
+import * as InfiniteScroll from 'react-infinite-scroller';
+
 import { MovieGrid } from './movie-grid';
 import { Movie } from '../domains/movie';
 import { API_KEY } from '../domains/api';
 
-import { HttpClient } from 'lupa/utils/http-client';
-
 export class ExplorePage extends React.Component<{}, { movies: Movie[] }> {
 
   movies: Movie[] = [];
+  fetchOptions = {
+    language: 'en-US',
+    sort_by: 'popularity.desc',
+    include_adult: false,
+    include_video: false,
+    page: 1,
+    api_key: API_KEY,
+  };
 
   onClickCard() {
     alert('card clicked');
@@ -22,30 +31,36 @@ export class ExplorePage extends React.Component<{}, { movies: Movie[] }> {
   }
 
   componentDidMount() {
-    const endpoint = 'https://api.themoviedb.org/3/discover/movie';
-    const option = {
-      language: 'en-US',
-      sort_by: 'popularity.desc',
-      include_adult: false,
-      include_video: false,
-      page: 1,
-      api_key: API_KEY,
-    };
-    HttpClient.getInstance().fetch(endpoint, option)
-      .then((data: any) => this.onGetMoviesDone(data))
-      .catch();
-
+    this.fetchMovies();
   }
 
-  onGetMoviesDone(data: any) {
+  fetchMovies() {
+    const endpoint = 'https://api.themoviedb.org/3/discover/movie';
+
+    HttpClient.getInstance().fetch(endpoint, this.fetchOptions)
+      .then((data: any) => this.onFetchMoviesDone(data))
+      .catch();
+  }
+
+  onFetchMoviesDone(data: any) {
     data.results.forEach((result: any) => {
       const movie = Movie.createFromResponse(result);
       this.movies.push(movie);
     });
     this.setState({ movies: [...this.movies] });
+    this.fetchOptions.page += 1;
   }
 
   render() {
-    return <MovieGrid movies={this.state.movies}/>;
+    return (
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={() => this.fetchMovies()}
+        hasMore={true || false}
+        loader={<div className="loader" key={0}>Loading ...</div>}
+      >
+        <MovieGrid movies={this.state.movies}/>;
+      </InfiniteScroll>
+    );
   }
 }
