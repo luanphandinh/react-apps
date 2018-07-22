f4379d85ae11b7fbdfa7bb87fd159b0f7ad3927d
Luan Phan <luan.phan@go1.com>
2018-07-22 17:05:48 +0700

    update http-clients on lupa/utils

---
+++ src/lupa/utils/http-client.ts
@@ -0,0 +1,35 @@
+export class HttpClient {
+  private static instance: HttpClient;
+
+  static getInstance(): HttpClient {
+    if (!this.instance) {
+      this.instance = new HttpClient();
+    }
+    return this.instance;
+  }
+
+  fetch(url: string, queryParams: any): Promise<any> {
+    const encodeParams = this.obj2UrlParams(queryParams);
+    const fetchUrl = `${url}?${encodeParams}`;
+
+    return fetch(fetchUrl)
+      .then(response => response.json())
+      .catch(error => error);
+  }
+
+  obj2UrlParams(obj: any) {
+    const pairs = [];
+
+    for (const prop in obj) {
+      if (obj.hasOwnProperty(prop)) {
+        const k = prop;
+        const v = encodeURIComponent(obj[prop]);
+        if (obj[prop] !== undefined) {
+          pairs.push(k + '=' + v);
+        }
+      }
+    }
+
+    return pairs.join('&');
+  }
+}
